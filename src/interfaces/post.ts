baf5aa60cac6a2416d8736df66c8db0057461226
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-09-05 12:22:19 +0900

    backup 24-09-05

--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -13,3 +13,21 @@ export type Post = {
   content: string;
   preview?: boolean;
 };
+
+
+interface PostDb {
+  post_id: number;
+  title: string;
+  excerpt?: string;
+  coverImage?: string;
+  date: Date;
+  author_id: number;
+  ogImage?: string;
+  view_cnt: number;
+  content: string; // post content가 있다고 가정합니다
+  preview: boolean; // 알림 preview 정보 (예상되는 필드 추가)
+  author: {
+    name: string;
+    profileImage?: string;
+  }; // author 정보도 추가
+}
