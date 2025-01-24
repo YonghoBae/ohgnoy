9e66a6f4db60c1ff7038e2eb11c02d5c9e7129a2
YonghoBae <dyddyd134@chungbuk.ac.kr>
2025-01-24 16:23:21 +0900

    style:css update

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -26,7 +26,7 @@ export function PostPreview({
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className="text-2xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`} className="hover:underline">
           {title}
         </Link>
@@ -34,8 +34,8 @@ export function PostPreview({
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      {/* <p className="text-lg leading-relaxed mb-4">{excerpt}</p> */}
+      {/* <Avatar name={author.name} picture={author.picture} /> */}
     </div>
   );
 }
